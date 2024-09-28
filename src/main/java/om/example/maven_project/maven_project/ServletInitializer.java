95758048773789287cc8b1c5ed414431aaa437a2
sudarshan <sudarshansw7@gmail.com>
2024-09-28 11:45:55 +0530

    a

---
+++ src/main/java/om/example/maven_project/maven_project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package om.example.maven_project.maven_project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MavenProjectApplication.class);
+	}
+
+}
---
+++ src/test/java/om/example/maven_project/maven_project/MavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package om.example.maven_project.maven_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
